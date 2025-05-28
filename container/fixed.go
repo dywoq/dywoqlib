59819fd5b7d5dd5a60f561b93c3088ba31d5b038
dywoq <aleks.koyf@gmail.com>
2025-05-28 19:49:02 +0300

    [container] fix inline documentation of OffInitialLength

--- container/fixed.go
+++ container/fixed.go
@@ -18,7 +18,7 @@ func NewFixed[T any](initialLength int, data []T) *Fixed[T] {
 	return &Fixed[T]{initialLength, data}
 }
 
-// OffLimit checks if the fixed-length slice is off the initial length.
+// OffInitialLength checks if the fixed-length slice is off the initial length.
 func (f Fixed[T]) OffInitialLength() bool {
 	return f.ActualLength() >= f.InitialLength()
 }
