af958348e0353a9f576e3ef73a0bbe155fd09b02
dywoq <aleks.koyf@gmail.com>
2025-06-09 16:02:12 +0300

    [attribute] add Deprecated and DeprecatedN functions

--- attribute/todo.go
+++ attribute/todo.go
@@ -1,10 +1,14 @@
-package attributes
+package attribute
 
 import "strings"
 
 // Todo is used to output warning into console to user,
 // who tries to use an unimplemented function.
 // Does not returns zero values automatically (e.g., nil, 0, "").
+//
+// If mode is set to SoftMode, then program will be not executed.
+// Otherwise it's set to StrictMode, program will be executed.
+//
 // Example:
 // 	func NotImplemented() int {
 //		attributes.Todo(attributes.SoftMode)
