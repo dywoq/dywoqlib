24fcbb674d8bc4a009f4c53273a04521561d9912
dywoq <aleks.koyf@gmail.com>
2025-06-13 13:50:17 +0300

    [slice/iterator] update errors' format

--- slice/iterator/errors.go
+++ slice/iterator/errors.go
@@ -19,8 +19,8 @@ import "errors"
 // ErrOutOfBounds indicates an attempt to access an element outside the valid
 // range of the iterator's data. This error occurs when trying to retrieve a value
 // at an invalid position.
-var ErrOutOfBounds = errors.New("slice.iterator.Iterator: not within bounds after Next()")
+var ErrOutOfBounds = errors.New("github.com/dywoq/dywoqlib/slice/iterator: not within bounds after Next()")
 
 // ErrNoMoreElements signifies that the iterator has reached the end of its
 // data collection and no further elements are available for iteration.
-var ErrNoMoreElements = errors.New("slice.iterator.Iterator: no more elements to iterate")
+var ErrNoMoreElements = errors.New("github.com/dywoq/dywoqlib/slice/iterator: no more elements to iterate")
