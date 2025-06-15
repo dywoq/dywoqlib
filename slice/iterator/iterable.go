881bc86f8011925124f69f2cf8e253f99265f6e9
dywoq <aleks.koyf@gmail.com>
2025-06-15 12:05:25 +0300

    [global] remove package slice

--- slice/iterator/iterable.go
+++
@@ -1,7 +0,0 @@
-package iterator
-
-// Iterable interface for types that can provide iterators for traversal.
-type Iterable[T comparable] interface {
-	Begin() Iterator[T]
-	End() Iterator[T]
-}
--- slice/iterator/new.go
+++
@@ -1,7 +0,0 @@
-package iterator
-
-// New returns a new instance of the structure Iterator.
-// The generic type must be given.
-func New[T comparable](pos int, slice []T) Iterator[T] {
-	return Iterator[T]{pos: pos, data: slice, err: nil}
-}
