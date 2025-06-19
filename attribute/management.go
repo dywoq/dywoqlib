c886147246cfdbd94b0430175932d4c9bef59282
dywoq <aleks.koyf@gmail.com>
2025-06-19 09:14:49 +0300

    [attribute] write nil-check for f

--- attribute/management.go
+++ attribute/management.go
@@ -6,13 +6,16 @@ import (
 
 type management struct{}
 
-func (management) funcInfo(skip int) (string) {
+func (management) funcInfo(skip int) string {
 	ret, _, _, ok := runtime.Caller(skip)
 	if !ok {
 		return "unknown"
 	}
-	fn := runtime.FuncForPC(ret)
-	name := fn.Name()
+	f := runtime.FuncForPC(ret)
+	if f == nil {
+		return "unknown"
+	}
+	name := f.Name()
 	return name
 }
 
@@ -21,4 +24,3 @@ func (management) skipNums() (int, int) {
 	// 3 - skip num of source of the warning
 	return 2, 3
 }
-
