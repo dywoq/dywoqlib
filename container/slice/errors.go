adc699149f3899e55610925e6670cbf6e5456db5
dywoq <aleks.koyf@gmail.com>
2025-06-20 11:43:36 +0300

    [slice] add Fixed

--- container/slice/errors.go
+++ container/slice/errors.go
@@ -3,3 +3,4 @@ package slice
 import "errors"
 
 var ErrSliceIsEmpty = errors.New("github.com/dywoq/dywoqlib/container/slice: slice is empty")
+var ErrOverFixedSize = errors.New("github.com/dywoq/dywoqlib/container/slice: over fixed size")
---
+++ container/slice/fixed.go
@@ -0,0 +1,181 @@
+package slice
+
+import (
+	"slices"
+
+	"github.com/dywoq/dywoqlib/iterator"
+	"github.com/dywoq/dywoqlib/sliceutil"
+)
+
+type Fixed[T comparable] struct {
+	s         []T
+	err       error
+	fixedSize int
+}
+
+func NewFixed[T comparable](args ...T) *Dynamic[T] {
+	return &Dynamic[T]{args, nil}
+}
+
+func (f *Fixed[T]) Length() int {
+	return len(f.s)
+}
+
+func (f *Fixed[T]) FixedSize() int {
+	return f.fixedSize
+}
+
+func (f *Fixed[T]) Err() error {
+	return f.err
+}
+
+func (f *Fixed[T]) Begin() *iterator.Iterator[T] {
+	return iterator.New(0, f.s)
+}
+
+func (f *Fixed[T]) End() *iterator.Iterator[T] {
+	return iterator.New(len(f.s)-1, f.s)
+}
+
+func (f *Fixed[T]) Find(reqElem T) T {
+	if f.err != nil {
+		var zero T
+		return zero
+	}
+	if f.overFixedSize() {
+		f.err = ErrOverFixedSize
+		var zero T
+		return zero
+	}
+	m := sliceutil.Management[T]{}
+	m.SetIterableType(f)
+	foundElem := m.Find(reqElem)
+	if m.Err() != nil {
+		f.err = m.Err()
+		var zero T
+		return zero
+	}
+	return foundElem
+}
+
+func (f *Fixed[T]) At(i int) T {
+	if f.err != nil {
+		var zero T
+		return zero
+	}
+	if f.overFixedSize() {
+		f.err = ErrOverFixedSize
+		var zero T
+		return zero
+	}
+	m := sliceutil.Management[T]{}
+	m.SetIterableType(f)
+	foundElem := m.At(i)
+	if m.Err() != nil {
+		f.err = m.Err()
+		var zero T
+		return zero
+	}
+	return foundElem
+}
+
+func (f *Fixed[T]) String() string {
+	m := sliceutil.Management[T]{}
+	m.SetIterableType(f)
+	return m.Format()
+}
+
+func (f *Fixed[T]) Front() T {
+	if f.err != nil {
+		var zero T
+		return zero
+	}
+	if f.overFixedSize() {
+		f.err = ErrOverFixedSize
+		var zero T
+		return zero
+	}
+	if (len(f.s)) == 0 {
+		f.err = ErrSliceIsEmpty
+		var zero T
+		return zero
+	}
+	return f.At(0)
+}
+
+func (f *Fixed[T]) Back() T {
+	if f.err != nil {
+		var zero T
+		return zero
+	}
+	if f.overFixedSize() {
+		f.err = ErrOverFixedSize
+		var zero T
+		return zero
+	}
+	if (len(f.s)) == 0 {
+		f.err = ErrSliceIsEmpty
+		var zero T
+		return zero
+	}
+	return f.At(f.Length() - 1)
+}
+
+func (f *Fixed[T]) AppendBack(args ...T) []T {
+	if f.err != nil {
+		var zero []T
+		return zero
+	}
+	if (len(f.s)) == 0 {
+		f.err = ErrSliceIsEmpty
+		var zero []T
+		return zero
+	}
+	f.s = append(f.s, args...)
+	return args
+}
+
+func (f *Fixed[T]) Append(args ...T) {
+	if f.err != nil {
+		return
+	}
+	_ = f.AppendBack(args...)
+}
+
+func (f *Fixed[T]) PopBack() T {
+	if f.err != nil {
+		var zero T
+		return zero
+	}
+	if (len(f.s)) == 0 {
+		f.err = ErrSliceIsEmpty
+		var zero T
+		return zero
+	}
+	i := len(f.s) - 1
+	elem := f.s[i]
+	f.s = slices.Delete(f.s, i, len(f.s))
+	return elem
+}
+
+func (f *Fixed[T]) Pop() {
+	if f.err != nil {
+		return
+	}
+	_ = f.PopBack()
+}
+
+func (f *Fixed[T]) Erase() {
+	if f.err != nil {
+		return
+	}
+	if (len(f.s)) == 0 {
+		f.err = ErrSliceIsEmpty
+		return
+	}
+	f.s = []T{}
+}
+
+func (f *Fixed[T]) overFixedSize() bool {
+	return len(f.s) > f.fixedSize
+}
