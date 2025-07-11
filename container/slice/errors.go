eedbc7417b945fcaa1669f8ba427cb05c9a17eca
dywoq <aleks.koyf@gmail.com>
2025-07-11 14:18:44 +0300

    [container/slice] add Fixed structure

---
+++ container/slice/errors.go
@@ -0,0 +1,8 @@
+package slice
+
+import "errors"
+
+var (
+	ErrNegativeFixedLength    = errors.New("github.com/dywoq/dywoqlib/container/slice: negative fixed length")
+	ErrFixedLengthOutOfBounds = errors.New("github.com/dywoq/dywoqlib/container/slice: fixed length is out of bounds")
+)
---
+++ container/slice/fixed.go
@@ -0,0 +1,125 @@
+package slice
+
+import "github.com/dywoq/dywoqlib/iterator"
+
+type Fixed[T comparable] struct {
+	err      error
+	fixedLen int
+	d        *Dynamic[T]
+}
+
+func NewFixed[T comparable](fixedLen int, s []T) *Fixed[T] {
+	err := error(nil)
+	if fixedLen < 0 {
+		err = ErrNegativeFixedLength
+	}
+	if len(s) < fixedLen {
+		err = ErrFixedLengthOutOfBounds
+	}
+	d := NewDynamic(s)
+	if d.Error() != nil {
+		err = d.Error()
+	}
+	return &Fixed[T]{err, fixedLen, d}
+}
+
+func (f *Fixed[T]) Error() error {
+	return f.err
+}
+
+func (f *Fixed[T]) Length() int {
+	return f.d.Length()
+}
+
+func (f *Fixed[T]) Iterating() *iterator.Combined[T] {
+	return f.Iterating()
+}
+
+func (f *Fixed[T]) Append(elems ...T) []T {
+	if f.err != nil {
+		return []T{}
+	}
+	appended := f.Append(elems...)
+	if f.d.Error() != nil {
+		f.err = f.d.Error()
+		return []T{}
+	}
+	return appended
+}
+
+func (f *Fixed[T]) At(i int) T {
+	if f.err != nil {
+		return f.zero()
+	}
+	got := f.d.At(i)
+	if f.d.Error() != nil {
+		f.err = f.d.Error()
+		return f.zero()
+	}
+	return got
+}
+
+func (f *Fixed[T]) Find(req T) T {
+	if f.err != nil {
+		return f.zero()
+	}
+	found := f.d.Find(req)
+	if f.d.Error() != nil {
+		f.err = f.d.Error()
+		return f.zero()
+	}
+	return found
+}
+
+func (f *Fixed[T]) String() string {
+	if f.err != nil {
+		return ""
+	}
+	formatted := f.d.String()
+	if f.d.Error() != nil {
+		f.err = f.d.Error()
+		return ""
+	}
+	return formatted
+}
+
+func (f *Fixed[T]) Set(elem T, i int) T {
+	if f.err != nil {
+		return f.zero()
+	}
+	new := f.d.Set(elem, i)
+	if f.d.Error() != nil {
+		f.err = f.d.Error()
+		return f.zero()
+	}
+	return new
+}
+
+func (f *Fixed[T]) Delete(i int) T {
+	if f.err != nil {
+		return f.zero()
+	}
+	deleted := f.d.Delete(i)
+	if f.d.Error() != nil {
+		f.err = f.d.Error()
+		return f.zero()
+	}
+	return deleted
+}
+
+func (f *Fixed[T]) Insert(i int, elem T) T {
+	if f.err != nil {
+		return f.zero()
+	}
+	inserted := f.d.Insert(i, elem)
+	if f.d.Error() != nil {
+		f.err = f.d.Error()
+		return f.zero()
+	}
+	return inserted
+}
+
+func (f *Fixed[T]) zero() T {
+	var zero T
+	return zero
+}
