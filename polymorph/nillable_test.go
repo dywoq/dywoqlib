84d124fdd3edde822482688f2e0093546097d78c
dywoq <aleks.koyf@gmail.com>
2025-07-19 10:48:59 +0300

    [polymorph] write tests for Nillable

---
+++ polymorph/nillable_test.go
@@ -0,0 +1,23 @@
+package polymorph
+
+import "testing"
+
+func TestNillable(t *testing.T) {
+	tests := []struct {
+		body      string
+		got, want bool
+	}{
+		{"Nillable[chan int]()", Nillable[chan int](), true},
+		{"Nillable[func(string)]()", Nillable[func(string)](), true},
+		{"Nillable[interface { A() string }]()", Nillable[interface{ A() string }](), true},
+		{"Nillable[map[int]int]()", Nillable[map[int]int](), true},
+		{"Nillable[*int]()", Nillable[*int](), true},
+		{"Nillable[[]int]()", Nillable[[]int](), true},
+	}
+
+	for _, test := range tests {
+		if test.got != test.want {
+			t.Errorf("%s = %v, %v", test.body, test.got, test.want)
+		}
+	}
+}
