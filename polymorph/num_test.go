afad19653908ba4b110526e58879454e74ce84e8
dywoq <aleks.koyf@gmail.com>
2025-07-19 10:53:25 +0300

    [polymorph] add tests for num.go

---
+++ polymorph/num_test.go
@@ -0,0 +1,45 @@
+package polymorph
+
+import "testing"
+
+type numStruct1 struct{}
+
+func (numStruct1) A() {}
+func (numStruct1) B() {}
+
+type numStruct2 struct {
+	A string
+	B int
+}
+
+func TestNumMethods(t *testing.T) {
+	tests := []struct {
+		body      string
+		got, want int
+	}{
+		{"NumMethods[numStruct1]()", NumMethods[numStruct1](), 2},
+		{"NumMethods[numStruct2]()", NumMethods[numStruct2](), 0},
+	}
+
+	for _, test := range tests {
+		if test.got != test.want {
+			t.Errorf("%s = %v, want %v", test.body, test.got, test.want)
+		}
+	}
+}
+
+func TestNumFields(t *testing.T) {
+	tests := []struct {
+		body      string
+		got, want int
+	}{
+		{"NumFields[numStruct1]()", NumFields[numStruct1](), 0},
+		{"NumFields[numStruct2]()", NumFields[numStruct2](), 2},
+	}
+
+	for _, test := range tests {
+		if test.got != test.want {
+			t.Errorf("%s = %v, want %v", test.body, test.got, test.want)
+		}
+	}
+}
