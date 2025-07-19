546c2b355903ebf88f079b004a5d8ca0c03ae428
dywoq <aleks.koyf@gmail.com>
2025-07-19 10:20:19 +0300

    [polymorph] write tests for Implements

---
+++ polymorph/implements_test.go
@@ -0,0 +1,31 @@
+package polymorph
+
+import "testing"
+
+type someInterfaceForV interface {
+	A() string
+}
+
+type v struct{} // implements someInterfaceForV
+type g struct{} // doesn't implement someInterfaceForV
+
+func (v) A() string { return "A" }
+
+func TestImplements(t *testing.T) {
+	tests := []struct {
+		body      string
+		got, want bool
+	}{
+		{"Implements[someInterfaceForV, v]()", Implements[someInterfaceForV, v](), true},
+		{"Implements[someInterfaceForV, g]()", Implements[someInterfaceForV, g](), false},
+		// Implements expects I to be an interface.
+		// If it is not, Implements returns false.
+		{"Implements[g, g]()", Implements[g, g](), false},
+	}
+
+	for _, test := range tests {
+		if test.got != test.want {
+			t.Errorf("%v = %v, want %v", test.body, test.got, test.want)
+		}
+	}
+}
