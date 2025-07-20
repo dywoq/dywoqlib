6270c4da7e86a9b195423945ad0ce10cf74f184e
dywoq <aleks.koyf@gmail.com>
2025-07-20 11:10:52 +0300

    [container/atd] add inline code documentation

--- container/atd/fifo.go
+++ container/atd/fifo.go
@@ -2,11 +2,14 @@ package atd
 
 import "github.com/dywoq/dywoqlib/container/slice"
 
+// Fifo represents a generic first-in-first-out (FIFO) queue.
+// It stores elements of any comparable type T and provides queue operations.
 type Fifo[T comparable] struct {
 	err error
 	d   *slice.Dynamic[T]
 }
 
+// NewFifo creates and returns a new instance of Fifo for elements of type T.
 func NewFifo[T comparable]() *Fifo[T] {
 	d := slice.NewDynamic[T]()
 	if d.Error() != nil {
@@ -15,22 +18,31 @@ func NewFifo[T comparable]() *Fifo[T] {
 	return &Fifo[T]{nil, d}
 }
 
+// Native returns the underlying slice of elements stored in the Fifo.
+// This provides direct access to the internal data representation.
 func (f *Fifo[T]) Native() []T {
 	return f.d.Native()
 }
 
+// Error returns the last error encountered by the Fifo instance.
+// If no error has occurred, it returns nil.
 func (f *Fifo[T]) Error() error {
 	return f.err
 }
 
+// Empty returns true if the FIFO queue contains no elements, otherwise false.
 func (f *Fifo[T]) Empty() bool {
 	return f.d.Length() == 0
 }
 
+// Length returns the number of elements currently stored in the Fifo queue.
 func (f *Fifo[T]) Length() int {
 	return f.d.Length()
 }
 
+// Front returns the element at the front of the FIFO queue without removing it.
+// If an internal error has occurred, it returns the zero value of T.
+// If an error is encountered when accessing the underlying data structure, it sets the error state and returns the zero value of T.
 func (f *Fifo[T]) Front() T {
 	if f.err != nil {
 		return f.zero()
@@ -43,6 +55,9 @@ func (f *Fifo[T]) Front() T {
 	return res
 }
 
+// Back returns the last element in the FIFO queue without removing it.
+// If an error has previously occurred or an error is encountered while accessing
+// the underlying data structure, Back returns the zero value of type T.
 func (f *Fifo[T]) Back() T {
 	if f.err != nil {
 		return f.zero()
@@ -55,6 +70,10 @@ func (f *Fifo[T]) Back() T {
 	return res
 }
 
+// Append adds the specified element to the FIFO queue.
+// If the FIFO is in an error state, it returns the zero value of T.
+// If an error occurs during the append operation, the error is stored in the FIFO
+// and the zero value of T is returned. Otherwise, it returns the appended element.
 func (f *Fifo[T]) Append(elem T) T {
 	if f.err != nil {
 		return f.zero()
@@ -67,6 +86,9 @@ func (f *Fifo[T]) Append(elem T) T {
 	return res[0]
 }
 
+// Pop removes and returns the front element from the FIFO queue.
+// If an internal error has occurred previously, or if an error occurs during the pop operation,
+// it returns the zero value of type T and sets the error state accordingly.
 func (f *Fifo[T]) Pop() T {
 	if f.err != nil {
 		return f.zero()
@@ -79,6 +101,8 @@ func (f *Fifo[T]) Pop() T {
 	return res
 }
 
+// String returns the string representation of the Fifo.
+// If there is an error present in the Fifo, it returns an empty string.
 func (f *Fifo[T]) String() string {
 	if f.err != nil {
 		return ""
--- container/atd/lifo.go
+++ container/atd/lifo.go
@@ -2,11 +2,16 @@ package atd
 
 import "github.com/dywoq/dywoqlib/container/slice"
 
+// Lifo represents a generic Last-In-First-Out (LIFO) stack data structure.
+// It stores elements of any comparable type T and provides stack operations.
 type Lifo[T comparable] struct {
 	err error
 	d   *slice.Dynamic[T]
 }
 
+// NewLifo creates and returns a new instance of Lifo[T], a last-in-first-out (LIFO) stack.
+// It initializes the underlying dynamic slice for storing elements of type T.
+// If an error occurs during initialization, the returned Lifo will contain the error and a nil data slice.
 func NewLifo[T comparable]() *Lifo[T] {
 	d := slice.NewDynamic[T]()
 	if d.Error() != nil {
@@ -15,22 +20,33 @@ func NewLifo[T comparable]() *Lifo[T] {
 	return &Lifo[T]{nil, d}
 }
 
+// Native returns the underlying slice of elements stored in the Lifo.
+// This provides direct access to the internal data structure.
 func (l *Lifo[T]) Native() []T {
 	return l.d.Native()
 }
 
+// Error returns the last error encountered by the Lifo instance.
+// If no error has occurred, it returns nil.
 func (l *Lifo[T]) Error() error {
 	return l.err
 }
 
+// Length returns the number of elements currently stored in the Lifo stack.
 func (l *Lifo[T]) Length() int {
 	return l.d.Length()
 }
 
+// Empty returns true if the Lifo stack contains no elements.
 func (l *Lifo[T]) Empty() bool {
 	return l.d.Length() == 0
 }
 
+// Append adds the given element to the Lifo container.
+// If the Lifo is in an error state, it returns the zero value of T.
+// If an error occurs during the append operation, the error is stored in the Lifo
+// and the zero value of T is returned.
+// Otherwise, it returns the appended element.
 func (l *Lifo[T]) Append(elem T) T {
 	if l.err != nil {
 		return l.zero()
@@ -43,6 +59,9 @@ func (l *Lifo[T]) Append(elem T) T {
 	return res[0]
 }
 
+// Pop removes and returns the top element from the Lifo stack.
+// If an error has previously occurred or an error occurs during the pop operation,
+// it returns the zero value of type T and sets the error state.
 func (l *Lifo[T]) Pop() T {
 	if l.err != nil {
 		return l.zero()
@@ -55,6 +74,9 @@ func (l *Lifo[T]) Pop() T {
 	return res
 }
 
+// Top returns the element at the top of the LIFO stack without removing it.
+// If the LIFO is in an error state or an error occurs while accessing the top element,
+// it returns the zero value of type T.
 func (l *Lifo[T]) Top() T {
 	if l.err != nil {
 		return l.zero()
@@ -67,6 +89,8 @@ func (l *Lifo[T]) Top() T {
 	return res
 }
 
+// String returns the string representation of the Lifo stack.
+// If the Lifo has an error, it returns an empty string.
 func (l *Lifo[T]) String() string {
 	if l.err != nil {
 		return ""
