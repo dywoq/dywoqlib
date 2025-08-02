d8c423df9b3e5d041e9e893a3cb66977c2b24126
dywoq <aleks.koyf@gmail.com>
2025-08-02 13:33:32 +0300

    [container\slice] add benchmarks for merge functions

--- container/slice/merge_test.go
+++ container/slice/merge_test.go
@@ -6,8 +6,10 @@ import (
 )
 
 func TestMergeDynamic(t *testing.T) {
-	first := NewDynamic(1, 2)
-	second := NewDynamic(3, 4)
+	var (
+		first  = NewDynamic(1, 2)
+		second = NewDynamic(3, 4)
+	)
 	got, err := MergeDynamic(first, second)
 	if err != nil {
 		t.Fatal(err)
@@ -19,14 +21,15 @@ func TestMergeDynamic(t *testing.T) {
 }
 
 func TestMergeFixed(t *testing.T) {
-	first := NewFixed(10, 1, 2)
+	var (
+		first  = NewFixed(10, 1, 2)
+		second = NewFixed(10, 3, 4)
+	)
 	if first.Error() != nil {
-		t.Fatal(first.Error())
+		t.Error(first.Error())
 	}
-
-	second := NewFixed(10, 3, 4)
 	if second.Error() != nil {
-		t.Fatal(second.Error())
+		t.Error(second.Error())
 	}
 
 	got, err := MergeFixed(first, second)
@@ -44,11 +47,43 @@ func TestMergeFixed(t *testing.T) {
 }
 
 func TestMerge(t *testing.T) {
-	first := []int{10, 54, 3}
-	second := []int{89, 23, 10}
-	got := Merge(first, second)
-	want := []int{10, 54, 3, 89, 23, 10}
+	var (
+		first = []int{10, 54, 3}
+		second = []int{89, 23, 10}
+		got = Merge(first, second)
+		want = []int{10, 54, 3, 89, 23, 10}
+	)
 	if !slices.Equal(got, want) {
 		t.Errorf("Merge(first, second) = %v, want %v", got, want)
+	}
+}
+
+func BenchmarkMergeDynamic(b *testing.B) {
+	var (
+		first  = NewDynamic(10, 1, 2)
+		second = NewDynamic(10, 3, 4)
+	)
+	for b.Loop() {
+		_, _ = MergeDynamic(first, second)
+	}
+}
+
+func BenchmarkMergeFixed(b *testing.B) {
+	var (
+		first  = NewFixed(10, 1, 2)
+		second = NewFixed(10, 3, 4)
+	)
+	for b.Loop() {
+		_, _ = MergeFixed(first, second)
+	}
+}
+
+func BenchmarkMerge(b *testing.B) {
+	var (
+		first = []int{10, 54, 3}
+		second = []int{89, 23, 10}
+	)
+	for b.Loop() {
+		_ = Merge(first, second)
 	}
 }
