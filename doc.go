287bcf70f93e76db1166794a2aad7a26e52b4776
dywoq <aleks.koyf@gmail.com>
2025-08-04 11:18:52 +0300

    [global] add doc.go

---
+++ doc.go
@@ -0,0 +1,13 @@
+// when Windows workflow runs, it sends this error:
+// Run go test -bench=. ./...
+// .
+// FAIL	. [setup failed]
+// no Go files in D:\a\dywoqlib\dywoqlib
+// FAIL
+// Error: Process completed with exit code 1.
+//
+// by adding doc.go, it makes the root directory a valid go package,
+// even it it doesn't contain any executable code. it allows to execute go test -bench=. ./...
+// without the "no Go files" error for the root.
+
+package dywoqlib
