dc6175bf02f84cba0a694e08eaadc8cd2428b8c3
dywoq <aleks.koyf@gmail.com>
2025-08-05 21:33:40 +0300

    [numeric] add Limits function

--- numeric/numeric.go
+++ numeric/numeric.go
@@ -1,8 +1,8 @@
-package integer
+package numeric
 
 // Numeric is a constraint of the integral, floating types (int, uint, float32, float64 etc.).
 type Numeric interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
-		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
-	~uintptr | ~float32 | ~float64
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~float32 | ~float64
 }
