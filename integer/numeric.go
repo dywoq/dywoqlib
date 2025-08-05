358dcdc8b9c79c11d1a273564700e9f9163964aa
dywoq <aleks.koyf@gmail.com>
2025-08-05 20:56:32 +0300

    [global] add integer package]

---
+++ integer/numeric.go
@@ -0,0 +1,8 @@
+package integer
+
+// Numeric is a constraint of the integral, floating types (int, uint, float32, float64 etc.).
+type Numeric interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+	~uintptr | ~float32 | ~float64
+}
