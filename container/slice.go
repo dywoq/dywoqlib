f2f23bb06763c6c55fefb637166e4fda1357e6d6
dywoq <aleks.koyf@gmail.com>
2025-08-27 22:48:14 +0300

    [container] add GrowableSlice type and its method Grow

--- container/slice.go
+++ container/slice.go
@@ -27,6 +27,10 @@ type IterableSlice[T comparable] []T
 // for more readable formatting of standard Go slice.
 type FormattableSlice[T comparable] []T
 
+// GrowableContainer is a slice of comparable generic parameter T
+// for safe pre-allocation of standard Go slice, implementing allocation.Sizer interface.
+type GrowableSlice[T comparable] []T
+
 // Format returns the formatted string of FormattableSlice[T].
 // Please notice the function uses sliceutil.Format(), which returns two values: the result and error;
 // the possible encountered error from sliceutil.Format() is ignored to align with fmt.Stringer interface.
@@ -36,3 +40,13 @@ func (f FormattableSlice[T]) String() string { str, _ := sliceutil.Format(f); re
 // Iterating returns a pointer to iterator.Combined[T] structure.
 // It uses a factory method iterator.NewCombined() internally.
 func (it IterableSlice[T]) Iterating() *iterator.Combined[T] { return iterator.NewCombined(it) }
+
+// Grow increases the capacity of the slice to at i.
+// If the current capacity is less than i, a new slice is allocated and elements are copied.
+func (g *GrowableSlice[T]) Grow(i int) {
+	if cap(*g) < i {
+		newSlice := make([]T, len(*g), i)
+		copy(newSlice, *g)
+		*g = newSlice
+	}
+}
