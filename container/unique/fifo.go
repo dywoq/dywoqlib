cc34ac372bb11da8d8be76999dcba07fb99750c6
dywoq <aleks.koyf@gmail.com>
2025-08-27 09:29:47 +0300

    [container/unique] add documentation in fifo.go

--- container/unique/fifo.go
+++ container/unique/fifo.go
@@ -6,31 +6,40 @@ import (
 	"github.com/dywoq/dywoqlib/err"
 )
 
+// Fifo is a generic first-in-first-out (FIFO) queue,
+// with only unique elements, using Slice internally.
 type Fifo[T comparable] struct {
 	s   *Slice[T]
 	err err.Context
 }
 
+// NewFifo creates and returns a new instance of Fifo for elements of type T.
 func NewFifo[T comparable]() *Fifo[T] {
 	return &Fifo[T]{NewSlice[T](), err.NoneContext()}
 }
 
+// Native returns the underlying slice of elements stored in the Fifo.
 func (f *Fifo[T]) Native() []T {
 	return f.s.Native()
 }
 
+// Error returns the possible encountered error context.
 func (f *Fifo[T]) Error() err.Context {
 	return f.err
 }
 
+// Empty checks whether the length of the underlying slice is 0.
 func (f *Fifo[T]) Empty() bool {
 	return f.s.Length() == 0
 }
 
+// Length returns the length of the underlying slice.
 func (f *Fifo[T]) Length() int {
 	return f.s.Length()
 }
 
+// Front returns the front element of the slice.
+// If Fifo error or the internal Slice error is not nil, it returns the zero value and sets the error.
 func (f *Fifo[T]) Front() T {
 	if !f.err.Nil() {
 		return f.zero()
@@ -44,6 +53,8 @@ func (f *Fifo[T]) Front() T {
 	return res
 }
 
+// Back returns the top element of the slice.
+// If Fifo error or the internal Slice error is not nil, it returns the zero value and sets the error.
 func (f *Fifo[T]) Back() T {
 	if !f.err.Nil() {
 		return f.zero()
@@ -57,6 +68,8 @@ func (f *Fifo[T]) Back() T {
 	return res
 }
 
+// Front appends the element to the slice, unless it already exists in the slice.
+// If Fifo error or the internal Slice error is not nil, it returns the zero value and sets the error.
 func (f *Fifo[T]) Append(elem T) T {
 	if !f.err.Nil() {
 		return f.zero()
@@ -75,6 +88,8 @@ func (f *Fifo[T]) Append(elem T) T {
 	return res[0]
 }
 
+// Pop removes the last element of the slice.
+// If Fifo error or the internal Slice error is not nil, it returns the zero value and sets the error.
 func (f *Fifo[T]) Pop() T {
 	if !f.err.Nil() {
 		return f.zero()
@@ -88,6 +103,8 @@ func (f *Fifo[T]) Pop() T {
 	return res
 }
 
+// String returns the formatted presentation of slice.
+// If Fifo error or the internal Slice error is not nil, it returns the empty string and sets the error.
 func (f *Fifo[T]) String() string {
 	if !f.err.Nil() {
 		return ""
