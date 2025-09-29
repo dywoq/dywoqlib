f5aa54e067ccd51eae6d8c3ab0710177e3d81b06
dywoq <aleks.koyf@gmail.com>
2025-09-29 21:22:45 +0300

    feature(filter): add functions Slice, Map

---
+++ filter/filter.go
@@ -0,0 +1,54 @@
+// filter provides a functionality to filter slices and maps.
+//
+// # Purpose
+//
+// The purpose is to reduce rewriting the same logic code.
+//
+// The developers have to rewrite the same logic of filtering slices and maps every time,
+// which is redundant and time-wasting. filter package solves this problem.
+//
+// # Example of using
+//
+//  package main
+//
+//  import (
+//    "fmt"
+//    "github.com/dywoq/dywoqlib/filter"
+//  )
+//
+//  func main() {
+//     s := filter.Slice([]int{1, 2, 3, 4, 5, 6}, func(elem int) bool { return elem%2 == 0 })
+//     fmt.Println(s)
+//  }
+//
+package filter
+
+// Slice returns a filtered slice of elements that satisfy pred.
+// Returns an empty slice if len(s) is 0.
+func Slice[S any](s []S, pred func(S) bool) []S {
+	if len(s) == 0 {
+		return []S{}
+	}
+	result := []S{}
+	for _, elem := range s {
+		if pred(elem) {
+			result = append(result, elem)
+		}
+	}
+	return result
+}
+
+// Map returns a filtered map of keys that satisfy pred.
+// Returns an empty map if len(m) is 0.
+func Map[K comparable, V any](m map[K]V, pred func(K, V) bool) map[K]V {
+	if len(m) == 0 {
+		return map[K]V{}
+	}
+	result := map[K]V{}
+	for k, v := range m {
+		if pred(k, v) {
+			result[k] = v
+		}
+	}
+	return result
+}
