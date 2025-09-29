e3f7aff9c71125c5ad4d98b13409a9e6828f9897
dywoq <aleks.koyf@gmail.com>
2025-09-29 21:39:37 +0300

    feature(filter): add MapKeys and MapValues

--- filter/map.go
+++ filter/map.go
@@ -20,3 +20,15 @@ func Map[K comparable, V any](m map[K]V, pred func(K, V) bool) map[K]V {
 func MapNot[K comparable, V any](m map[K]V, pred func(K, V) bool) map[K]V {
 	return Map(m, func(k K, v V) bool { return !pred(k, v) })
 }
+
+// MapKeys returns a filtered map with the keys that satisfy pred.
+// Returns an empty map if len(m) is 0.
+func MapKeys[K comparable, V any](m map[K]V, pred func(K) bool) map[K]V {
+	return Map(m, func(k K, v V) bool { return pred(k) })
+}
+
+// MapKeys returns a filtered map with the values, and their keys, that satisfy pred.
+// Returns an empty map if len(m) is 0.
+func MapValues[K comparable, V any](m map[K]V, pred func(V) bool) map[K]V {
+	return Map(m, func(k K, v V) bool { return pred(v) })
+}
