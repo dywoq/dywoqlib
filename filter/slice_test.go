a51f111d2917e8a89b5cf595b4e027af21a238e6
dywoq <aleks.koyf@gmail.com>
2025-09-30 11:26:29 +0300

    feature(filter): add benchmarks and examples in slice_test.go

--- filter/slice_test.go
+++ filter/slice_test.go
@@ -47,3 +47,72 @@ func TestSliceNot(t *testing.T) {
 		})
 	}
 }
+
+func BenchmarkSliceEven(b *testing.B) {
+	data := make([]int, 1000)
+	for i := range data {
+		data[i] = i
+	}
+	for i := 0; i < b.N; i++ {
+		_ = filter.Slice(data, func(i int) bool { return i%2 == 0 })
+	}
+}
+
+func BenchmarkSliceOdd(b *testing.B) {
+	data := make([]int, 1000)
+	for i := range data {
+		data[i] = i
+	}
+	for i := 0; i < b.N; i++ {
+		_ = filter.Slice(data, func(i int) bool { return i%2 != 0 })
+	}
+}
+
+func BenchmarkSliceNotEven(b *testing.B) {
+	data := make([]int, 1000)
+	for i := range data {
+		data[i] = i
+	}
+	for i := 0; i < b.N; i++ {
+		_ = filter.SliceNot(data, func(i int) bool { return i%2 == 0 })
+	}
+}
+
+func BenchmarkSliceNotOdd(b *testing.B) {
+	data := make([]int, 1000)
+	for i := range data {
+		data[i] = i
+	}
+	for i := 0; i < b.N; i++ {
+		_ = filter.SliceNot(data, func(i int) bool { return i%2 != 0 })
+	}
+}
+
+func ExampleSlice() {
+	data := []int{1, 2, 3, 4, 5, 6}
+	even := filter.Slice(data, func(i int) bool { return i%2 == 0 })
+	fmt.Println(even)
+	// Output: [2 4 6]
+}
+
+func ExampleSlice_odd() {
+	data := []int{1, 2, 3, 4, 5, 6}
+	odd := filter.Slice(data, func(i int) bool { return i%2 != 0 })
+	fmt.Println(odd)
+	// Output: [1 3 5]
+}
+
+func ExampleSliceNot() {
+	data := []int{1, 2, 3, 4, 5, 6}
+	notEven := filter.SliceNot(data, func(i int) bool { return i%2 == 0 })
+	fmt.Println(notEven)
+	// Output: [1 3 5]
+}
+
+func ExampleSliceNot_odd() {
+	data := []int{1, 2, 3, 4, 5, 6}
+	notOdd := filter.SliceNot(data, func(i int) bool { return i%2 != 0 })
+	fmt.Println(notOdd)
+	// Output: [2 4 6]
+}
+
